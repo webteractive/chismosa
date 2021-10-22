e5c5f96bd634747c0ca8ca3abce7946cfd1b28c4
Glen Bangkila <hadeinvane@gmail.com>
2021-10-22 23:38:54 +0800

    Add registration page.

--- routes/auth.php
+++ routes/auth.php
@@ -8,14 +8,17 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/register', [RegisteredUserController::class, 'create'])
-//                 ->middleware('guest')
-//                 ->name('register');
+if (User::count() == 0) {
+    Route::get('/register', [RegisteredUserController::class, 'create'])
+                ->middleware('guest')
+                ->name('register');
 
-// Route::post('/register', [RegisteredUserController::class, 'store'])
-//                 ->middleware('guest');
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+                    ->middleware('guest');
+}
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
                 ->middleware('guest')
