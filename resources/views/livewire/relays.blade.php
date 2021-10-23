dd8cbfddebc7ef15c097a076c1ffc6894bc36b19
Glen Bangkila <hadeinvane@gmail.com>
2021-10-23 16:20:50 +0800

    Relays UI upate

--- resources/views/livewire/relays.blade.php
+++ resources/views/livewire/relays.blade.php
@@ -1,31 +1,40 @@
-<div id="relays">
+<div id="relays" class="px-4 sm:px-0">
     <div class="flex justify-end">
         <x-button wire:click="$emitTo('relay-form', 'create')">{{ __('New Relay') }}</x-button>
     </div>
     
-    <div class="grid grid-cols-4 gap-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
         @foreach ($this->relays as $relay)
+            @php
+                $services = config('chismosa.services')
+            @endphp
             <div wire:key="relay-{{ $relay->id }}" class="rounded-lg shadow relative bg-gray-100">
                 <div class="p-4 bg-white border-b rounded-t-lg">
                     <h2 class="text-xl font-bold leading-none">{{ $relay->name }}</h2>
 
-                    <div class="space-y-3 mt-5">
+                    <div class="space-y-4 mt-5">
                         @if ($relay->description)
                             <p class="text-sm leading-tight">{{ $relay->description }}</p>
                         @endif
                         
-                        <div>
-                            <div>{{ __('Messages received from') }}</div>
+                        <div class="space-y-1">
+                            <div class="leading-none text-gray-800">
+                                <span>{{ __('Messages from') }}</span>
+                                <span class="text-black">{{ __($services[$relay->type]) }}</span>
+                            </div>
                             <div
-                                class="leading-none text-sm text-gray-600 hover:underline"
-                                x-data="clipboard"
-                                data-clipboard-text="{{ $relay->endpoint }}"
+                                class="leading-none text-sm text-blue-500 break-all font-mono"
                             >{{ $relay->endpoint }}</div>
                         </div>
 
-                        <div>
-                            <div>{{ __('Will be relayed to') }}</div>
-                            <div class="leading-none text-sm text-gray-600">{{ $relay->webhook_url }}</div>
+                        <div class="space-y-1">
+                            <div class="leading-none text-gray-800">
+                                <span>{{ __('Will be relayed to') }}</span>
+                                <span class="text-black">{{ __($services[$relay->webhook_type]) }}</span>
+                            </div>
+                            <div
+                                class="leading-none text-sm text-blue-500 break-all font-mono"
+                            >{{ $relay->webhook_url }}</div>
                         </div>
                     </div>
 
@@ -34,7 +43,7 @@
                     </div>
                 </div>
 
-                <div class="bg-gray-100 rounded-b-lg p-4 space-x-2">
+                <div class="bg-gray-100 rounded-b-lg p-4 flex justify-between items-center">
                     <button
                         type="button"
                         class="text-blue-500 hover:underline"
