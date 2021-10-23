6c90ebf76ac5353859bea41058d373eeab1d44c1
Glen Bangkila <hadeinvane@gmail.com>
2021-10-23 16:35:59 +0800

    Update relay logs UI

--- resources/views/livewire/relay-logs.blade.php
+++ resources/views/livewire/relay-logs.blade.php
@@ -1,15 +1,26 @@
 <x-modal
     :opened="$opened"
     :title="__(':name Logs', ['name' => optional($this->relay)->name])"
-    size="container"
+    size="w-full md:w-[720px]"
 >
-    <div class="logs">
-        @foreach ($this->logs as $item)
-            <pre>{{ json_encode($item) }}</pre>
-        @endforeach
-    </div>
+    @if ($this->relay)
+        <div class="logs -mx-4">
+            @foreach ($this->logs as $item)
+                <div class="border-b py-2 px-4 flex hover:bg-gray-50">
+                    <div class="flex-0">{{ $item->created_at->toDateTimeString() }}</div>
+                    <div class="flex-1 ml-4 break-all font-mono">{{ json_encode($item->payload) }}</div>
+                </div>
+            @endforeach
+        </div>
 
-    <div class="mt-6">
-        <x-button wire:click="close">{{ __('Close') }}</x-button>
-    </div>
+        @if ($this->logs->hasPages())
+            <div class="mt-2">
+                {{ $this->logs->links() }}
+            </div>
+        @endif
+
+        <div class="mt-6">
+            <x-button wire:click="close">{{ __('Close') }}</x-button>
+        </div>
+    @endif
 </x-modal>
